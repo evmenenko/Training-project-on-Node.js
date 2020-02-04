a56edf4f4301efae538ac594e1c8bfd94359ba75
Arthur Evmenenko <arthur.evmenenko@gmail.com>
2020-02-04 15:41:44 +0300

    updated code style

--- src/constants/errorsInfo.js
+++ src/constants/errorsInfo.js
@@ -1,42 +1,53 @@
 const errorsInfo = {
+
   "4xx": {
-    "badRequest": {
-      "message": "The request contains a syntax error",
-      "name": "BadRequest"
+
+    badRequest: {
+      message: "The request contains a syntax error",
+      name: "BadRequest"
     },
-    "conflict": {
-      "message": "The request could not be completed due to conflicting access to the resource",
-      "name": "Conflict"
+
+    conflict: {
+      message: "The request could not be completed due to conflicting access to the resource",
+      name: "Conflict"
     },
-    "gone": {
-      "message": "The resource has been deleted and is no longer available",
-      "name": "Gone"
+
+    gone: {
+      message: "The resource has been deleted and is no longer available",
+      name: "Gone"
     },
-    "notFound": {
-      "message": "The resource is not found",
-      "name": "NotFound"
+
+    notFound: {
+      message: "The resource is not found",
+      name: "NotFound"
     },
-    "retryWith": {
-      "message": "Did not enough information",
-      "name": "RetryWith"
+
+    retryWith: {
+      message: "Did not enough information",
+      name: "RetryWith"
     },
-    "unauthorized": {
-      "message": "To access the requested resource requires authentication",
-      "name": "Unauthorized"
+
+    unauthorized: {
+      message: "To access the requested resource requires authentication",
+      name: "Unauthorized"
     },
-    "unprocessableEntity": {
-      "message": "Server cannot process the request because there is a logical error in a sent data",
-      "name": "UnprocessableEntity"
+
+    unprocessableEntity: {
+      message: "Server cannot process the request because there is a logical error in a sent data",
+      name: "UnprocessableEntity"
     },
   },
+
   "5xx": {
-    "internalServerError": {
-      "message": "Internal server error",
-      "name": "InternalServerError"
+
+    internalServerError: {
+      message: "Internal server error",
+      name: "InternalServerError"
     },
-    "notImplemented": {
-      "message": "The server does not support capabilities required to process the request",
-      "name": "NotImplemented"
+    
+    notImplemented: {
+      message: "The server does not support capabilities required to process the request",
+      name: "NotImplemented"
     },
   },
 }
