596a8e0694117ac04868ac441dcffc1b6d07a681
Arthur Evmenenko <arthur.evmenenko@gmail.com>
2020-02-04 21:37:52 +0300

    updated sequelize

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,18 +1,24 @@
 const Sequelize = require('sequelize');
+const dbInfo = require('./config/database.json');
 
-const sequelize = new Sequelize("online_cinema_db", "root", "root", {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false
-});
+const sequelize = new Sequelize(
+  dbInfo.dev.database,
+  dbInfo.dev.username,
+  dbInfo.dev.password,
+  {
+    host: dbInfo.dev.host,
+    dialect: dbInfo.dev.dialect,
+    logging: dbInfo.dev.logging,
+  }
+);
 
 const connect = async () => {
-    await sequelize.authenticate();
-    await sequelize.sync();
+  await sequelize.authenticate();
+  await sequelize.sync();
 }
 
 module.exports = {
-    sequelize,
-    connect,
-    Sequelize,
+  sequelize,
+  connect,
+  Sequelize,
 }
